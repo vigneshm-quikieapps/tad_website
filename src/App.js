d9144de515c7d62e92d55d88f7354c38b6fdabdd
bishalquikieapps <bishal@quikieapps.com>
2022-03-10 18:25:07 +0530

    initial commit | created desktop 1 and desktop 3

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Desktop1MainContent from './Desktop1MainContent';
+import Desktop3PrivacyPolicy from './Desktop3PrivacyPolicy';
+import Footer from './Footer';
+import Header from './Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      {/* <Desktop1MainContent /> */}
+      <Desktop3PrivacyPolicy />
+      {/* <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './App.css';
+
+function Footer() {
+  return (
+    <>
+        <div className='Footer-content'>
+            <div className='Sub-Footer-content'>
+                <div className='Compoany-Footer'>
+                    <h5>Company</h5>
+                    <br/>
+                    <p>About TAD Group</p>
+                    <br/>
+                    <p>Newsroom</p>
+                    <br/>
+                    <p>Careers</p>
+                    <br/>
+                    <p>Contact Us</p>
+                </div>
+                <div className='Legal-Footer'>
+                    <h5>Legal</h5>
+                    <br/>
+                    <p>Privacy Policy</p>
+                    <br/>
+                    <p>Verification Policy</p>
+                    <br/>
+                    <p>Terms and Conditions Policy</p>
+                    <br/>
+                    <p>Community Guideline Policy</p>
+                    <br/>
+                    <p>End to End NDA Policy</p>
+                </div>
+            </div>
+            <hr style={{color:"white"}} />
+            <h2>THEARTDOOR</h2>
+            <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+        </div>
+    </>
+  )
+}
+
+export default Footer

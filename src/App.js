f2c8a71db139044758e70b33a7d0b93c2018df8b
bishalquikieapps <bishal@quikieapps.com>
2022-03-11 10:54:48 +0530

    added verification policy screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Desktop1MainContent from './Desktop1MainContent';
 import Desktop3PrivacyPolicy from './Desktop3PrivacyPolicy';
+import Desktop4VerificationPolicy from './Desktop4VerificationPolicy';
 import Footer from './Footer';
 import Header from './Header';
 
@@ -10,7 +11,8 @@ function App() {
     <div className="App">
       <Header />
       {/* <Desktop1MainContent /> */}
-      <Desktop3PrivacyPolicy />
+      {/* <Desktop3PrivacyPolicy /> */}
+      <Desktop4VerificationPolicy />
       {/* <Footer /> */}
     </div>
   );

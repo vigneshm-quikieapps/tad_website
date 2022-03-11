18f7ae5b70cbe6bfce4d40f3d8840bb776632a30
bishalquikieapps <bishal@quikieapps.com>
2022-03-11 11:59:02 +0530

    added community | end to end | terms and condition screens

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Desktop1MainContent from './Desktop1MainContent';
 import Desktop3PrivacyPolicy from './Desktop3PrivacyPolicy';
 import Desktop4VerificationPolicy from './Desktop4VerificationPolicy';
+import Desktop5CommunityGuidelinePolicy from './Desktop5CommunityGuidelinePolicy';
+import Desktop6EndtoEndPolicy from './Desktop6EndtoEndPolicy';
+import Desktop8TermsandCondition from './Desktop8TermsandCondition';
 import Footer from './Footer';
 import Header from './Header';
 
@@ -12,7 +15,10 @@ function App() {
       <Header />
       {/* <Desktop1MainContent /> */}
       {/* <Desktop3PrivacyPolicy /> */}
-      <Desktop4VerificationPolicy />
+      {/* <Desktop4VerificationPolicy /> */}
+      {/* <Desktop5CommunityGuidelinePolicy /> */}
+      {/* <Desktop6EndtoEndPolicy /> */}
+      <Desktop8TermsandCondition />
       {/* <Footer /> */}
     </div>
   );
